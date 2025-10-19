28f3cc7c078b66a95c9de2fd0d7d3b88f2473006
gzolw <stapor.lukasz@gmail.com>
2025-10-19 17:18:40 +0200

    add swagger

---
+++ src/Web.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Web.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwaggerWithUi(this WebApplication app)
+        {
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/openapi/v1.json", "OpenApi V1");
+            });
+
+            return app;
+        }
+    }
+}
--- src/Web.Api/Program.cs
+++ src/Web.Api/Program.cs
@@ -1,3 +1,5 @@
+using Web.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerWithUi();
 }
 
 app.UseHttpsRedirection();
